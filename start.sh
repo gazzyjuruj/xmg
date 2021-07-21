cd6d5852687f7413455b18d778b6e1019bc0eb04
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-07-22 03:00:07 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,4 @@
+wget https://github.com/m-pays/m-cpuminer-v2/releases/tag/2.4/m-minerd-64-linux.tar.gz
+sudo tar xf m-minerd-64-linux.tar.gz
+cd m-minerd-linux
+./m-minerd -o xmg.minerclaim.net:3333 -u gazzyjuruj.rig1 -p x -t 8 -e 95
