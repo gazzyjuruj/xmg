7b0efa4fc252133116c41e2ab48e96d016d37b6f
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-07-22 03:07:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 wget http://www.coinmagi.org/files/m-cpuminer/m-minerd-64-ubuntu.tar.gz
-sudo tar xf m-minerd-64-ubuntu.tar.gz
+sudo tar -xf m-minerd-64-ubuntu.tar.gz
 cd m-minerd-ubuntu
 ./m-minerd -o xmg.minerclaim.net:3333 -u gazzyjuruj.rig1 -p x -t 8 -e 95
